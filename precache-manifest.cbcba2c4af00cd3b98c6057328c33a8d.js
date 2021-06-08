d0e881e22ddf50a6da3eba2ea02b441b47babed7
Ephraim Elvis <ephraimelvis@Ephraims-MacBook-Pro.local>
2021-06-08 05:13:16 +0100

    Updates

---
+++ precache-manifest.cbcba2c4af00cd3b98c6057328c33a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33f6f3c8385723eda5514ad6c4b84d7",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "546e09bdc5a5321f1f2e",
+    "url": "/calculator-app/static/css/main.d7b37315.chunk.css"
+  },
+  {
+    "revision": "60e72ff555924f22203b",
+    "url": "/calculator-app/static/js/2.95d69990.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-app/static/js/2.95d69990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546e09bdc5a5321f1f2e",
+    "url": "/calculator-app/static/js/main.936bbf9a.chunk.js"
+  },
+  {
+    "revision": "e52fba28ccb6ce4c534d",
+    "url": "/calculator-app/static/js/runtime-main.0d33511c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-calculator/precache-manifest.101336308b1de8ceb54a24115acf1248.js"
+  "/calculator-app/precache-manifest.cbcba2c4af00cd3b98c6057328c33a8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculator-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
